4ad688baa7c1158865f1db5944310667d984c140
tingfengsiyu <228613006@qq.com>
2019-09-12 17:34:04 +0800

    error commit

--- app01/views.py
+++ app01/views.py
@@ -4,6 +4,7 @@ from app01 import models
 # Create your views here.
 def students(request):
     stu_list= models.Student.objects.all()
-    return  render(request,'students.html',{'stu_list':stu_list})
+    cls_list = models.Classes.objects.all()
+    return  render(request,'students.html',{'stu_list':stu_list,'cls_list':cls_list})
 
 
